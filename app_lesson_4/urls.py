56c1bb22361bc48005ffea6ec79fb1b7d1087098
Rodion <rodionshabalin3@gmail.com>
2023-07-28 12:13:04 +0300

    Занятие 5. ДЗ

--- app_lesson_4/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import hw
-
-urlpatterns = [
-    path('', hw)
-]
--- mysite/urls.py
+++ mysite/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('advertisements.urls')),
-    path('lesson_4/', include('app_lesson_4.urls'))
 ]
